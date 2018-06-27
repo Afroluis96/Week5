484b6878c2a13f56c21ad53ac1bcdb4c8dcc18fd
Luis Aparicio <luisaparicioj316@gmail.com>
2018-06-27 17:25:21 -0600

    WIP

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,26 @@
+const _ = require("lodash");
+const jwt = require('jsonwebtoken');
+const passport = require("passport");
+const passportJWT = require("passport-jwt");
+
+const ExtractJwt = passportJWT.ExtractJwt;
+const JwtStrategy = passportJWT.Strategy;
+
+const dbK = require('../db/index.js');
+const db = dbK();
+
+const users = db.import('../models/users');
+
+let allUsers;
+users.findAll().then(res =>{
+    allUsers = res;
+});
+
 
 
 module.exports = (app) =>{
-        app.post('user/login', (req,res) =>{
-            let user = req.body.idUser;
-            let pass = req.bofy.password;
+        app.get('/', (req,res) =>{
+           res.send(allUsers);
+
         })
 }
--- server.js
+++ server.js
@@ -3,9 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const {PORT} = process.env;
 const bodyParser = require('body-parser');
+const productController = require('./controllers/index');
 
 const app = express();
 app.use(bodyParser.json());
+
+productController(app);
 
 app.listen(PORT, ()=>{
     console.log("running on port: ",PORT);
