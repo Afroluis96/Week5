ba4d8ae4a62758e692810e295f08aaa74c195141
Luis Aparicio <luisaparicioj316@gmail.com>
2018-06-27 19:11:49 -0600

    Las minute changes

--- controllers/index.js
+++ controllers/index.js
@@ -18,7 +18,7 @@ users.findAll().then(res =>{
 
 var jwtOptions = {}
 jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-jwtOptions.secretOrKey = 'tasmanianDevil';
+jwtOptions.secretOrKey = 'applaudostudios';
 
 var strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
   console.log('payload received', jwt_payload);
@@ -31,14 +31,34 @@ var strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
   }
 });
 
-var s = 1;
 
 passport.use(strategy);
 
 
 module.exports = (app) =>{
         app.get('/', (req,res) =>{
-           res.send(allUsers);
+           res.json({message: "Express is up"});
 
-        })
+        });
+
+        app.post("/login", (req, res) => {
+            if(req.body.name && req.body.password){
+              var name = req.body.name;
+              var password = req.body.password;
+            }
+            // usually this would be a database call:
+            var user = users.findOne({whree:{userName:name}});
+            if( !user ){
+              res.status(401).send({message:"no such user found"});
+            }
+          
+            if(user.userPassword === password) {
+              // from now on we'll identify the user by the id and the id is the only personalized value that goes into our token
+              var payload = {id: user.id};
+              var token = jwt.sign(payload, jwtOptions.secretOrKey);
+              res.json({message: "ok", token: token});
+            } else {
+              res.status(401).json({message:"passwords did not match"});
+            }
+          });
 }
